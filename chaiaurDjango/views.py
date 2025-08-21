6a412244ba814799f7d52057c8f46b4538742465
PUNEET <44027672+Puneetbisarwal@users.noreply.github.com>
2025-08-21 17:27:01 +0530

    updated code for views.py file

--- chaiaurDjango/views.py
+++ chaiaurDjango/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
 
-def index(request):
+def home(request):
     return HttpResponse("Hello, world. You are at chai aur Django Home page")
+
+def about(request):
+    return HttpResponse("Hello, world. You are at chai aur Django About page")
+
+def contact(request):
+    return HttpResponse("Hello, world. You are at chai aur Django Contact page")
