40a6504f581dbf28d0771ec5a455fcbc48d71b77
PUNEET <44027672+Puneetbisarwal@users.noreply.github.com>
2025-08-21 17:53:19 +0530

    added website folder and moved index.html file

--- chaiaurDjango/views.py
+++ chaiaurDjango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     #return HttpResponse("Hello, world. You are at chai aur Django Home page")
-    return render(request, 'index.html')
+    return render(request, 'website/index.html')
 
 def about(request):
     return HttpResponse("Hello, world. You are at chai aur Django About page")
