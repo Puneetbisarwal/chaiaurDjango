44dc6227e9db13eda3e36e70436a7d315b1a64ff
PUNEET <44027672+Puneetbisarwal@users.noreply.github.com>
2025-08-27 17:44:35 +0530

    Updated chai_detail.html path and updated urls.py file.

--- chai/urls.py
+++ chai/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     path('', views.all_chai, name='all_chai'), 
     # path('order/', views.order_chai, name='order'),
+    path('<int:chai_id>/', views.chai_detail, name='chai_detail'),
 ]
