2712d27e6a1395201864d729a372c04997a1b4c8
PUNEET <44027672+Puneetbisarwal@users.noreply.github.com>
2025-08-28 13:21:34 +0530

    Updated urls.py and views.py and added code in chai_stores.html file.

--- chai/views.py
+++ chai/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import ChaiVariety
 from django.shortcuts import get_object_or_404
 
+
+
 # Create your views here.
 def all_chai(request):
     chais = ChaiVariety.objects.all()
@@ -10,3 +12,6 @@ def all_chai(request):
 def chai_detail(request, chai_id):
     chai = get_object_or_404(ChaiVariety, pk=chai_id)
     return render(request, 'chai/chai_detail.html', {'chai': chai})
+
+def chai_store_view(request):
+    return render(request, 'chai/chai_stores.html')
